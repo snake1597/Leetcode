51b0cc284f2a279fba31ab812ecce23a207ab45b
snake1597 <tombear07@gmail.com>
2019-08-29 18:25:41 +0800

    Delete TwoSum.py

--- TwoSum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = dict()
-        for i in range(0,len(nums)):
-            if target - nums[i] in table:
-                return [table[target - nums[i]],i]
-            table[nums[i]] = i
