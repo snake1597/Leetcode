c9f0d3979413fc5812be219d71ad0e8e3300b5ec
snake1597 <tombear07@gmail.com>
2019-09-02 14:11:44 +0800

    Create ContainsDuplicate.py

---
+++ Array/ContainsDuplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        table = dict()
+        for i in range(len(nums)):
+            if nums[i] in table:
+                return True
+            else:
+                table[nums[i]] = nums[i]
+        # use set()
+        #if not nums:
+			  #  return False
+		    #return len(set(nums)) != len(nums)
