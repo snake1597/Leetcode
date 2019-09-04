966fe94d322928ad5ca5f491d76ff9990ce5a2f9
snake1597 <tombear07@gmail.com>
2019-09-04 18:32:06 +0800

    Create ContainsDuplicateII.py

---
+++ Array/ContainsDuplicateII.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        table = dict()
+        for i in range(len(nums)):
+            if nums[i] in table and i - table[nums[i]] <= k:
+                    return True
+            else:    
+                table[nums[i]] = i
+        return False
